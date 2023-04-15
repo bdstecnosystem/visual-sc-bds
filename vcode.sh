78c8a23b789ffa63930be76ce1c46a71b72cf707
bdstecnosystem <112449384+bdstecnosystem@users.noreply.github.com>
2023-04-14 21:28:15 -0300

    Add files via upload

---
+++ vcode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+figlet -f "Dos Rebel" BDS
+sleep 5
+figlet V.CODE
+sleep 2
+echo "vamos instalar o visual code  "
+sleep 5
+clear
+sudo apt install code -y
+clear
+code
+firefox https://www.youtube.com/@bdstecnosystem
+#desenvolvido por @bdstecnosystem
